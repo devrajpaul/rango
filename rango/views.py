71900b25dc60579396bc692108dbba8c7972e725
devraj <devraj@devraj-home>
2015-06-18 23:57:02 +0530

    kl

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,8 @@ from rango.models import Category, Page
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_views_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'page_views': page_views_list}
 
     return render(request, 'rango/index.html', context_dict)
 
